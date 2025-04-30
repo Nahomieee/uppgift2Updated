88aeb999a7a0403db7d2da647fe65faee7353849
Nahomieee <146669902+Nahomieee@users.noreply.github.com>
2025-04-30 13:11:20 +0200

    .

--- src/test/java/stepdefinitions/MyStepdefs.java
+++ src/test/java/stepdefinitions/MyStepdefs.java
@@ -21,51 +21,65 @@ import java.util.Map;
 
 public class MyStepdefs {
 
-    WebDriver driver;
+    WebDriverWait wait;
+     WebDriver driver;
 
     @Given("I am on the right page")
     public void iAmOnTheRightPage() {
 
         driver = new ChromeDriver();
 
-        // driver = new SafariDriver();
+        //driver = new SafariDriver();
 
         driver.get("https://membership.basketballengland.co.uk/NewSupporterAccount");
     }
 
-    @When("I put all the login info")
-    public void iPutAllTheLoginInfo(DataTable table) throws InterruptedException {
+    @When("I put all the login info {string}, {string}, {string}, {string}, {string}, {string}")
+    public void iPutAllTheLoginInfo(String dateOfBirth, String forename, String surname, String email, String password, String confirmpassword) throws InterruptedException {
 
-        List<Map<String, String>> data = table.asMaps(String.class, String.class);
+        //List<Map<String, String>> data = table.asMaps(String.class, String.class);
 
+        //WebElement date = driver.findElement(By.name("14/02/2002"));
         WebElement date = driver.findElement(By.name("DateOfBirth"));
         //date.sendKeys(data.get(0).get("DateOfBirth"));
-        date.sendKeys("14/04/2002");
+        date.sendKeys(dateOfBirth);
+        Thread.sleep(1000);
 
+        //WebElement firstName = driver.findElement(By.name("Nahom"));
         WebElement firstName = driver.findElement(By.name("Forename"));
         //firstName.sendKeys(data.get(0).get("FirstName"));
-        firstName.sendKeys("Nahom");
+        firstName.sendKeys(forename);
+        Thread.sleep(1000);
 
+        //WebElement lastName = driver.findElement(By.name("Tesfay"));
         WebElement lastName = driver.findElement(By.name("Surname"));
         //lastName.sendKeys(data.get(0).get("LastName"));
-        lastName.sendKeys("Tesfay");
+        lastName.sendKeys(surname);
+        Thread.sleep(1000);
 
-        WebElement email = driver.findElement(By.name("EmailAddress"));
+        //WebElement mail = driver.findElement(By.name("Nahomt50@mailnesia.com"));
+        WebElement mail = driver.findElement(By.name("EmailAddress"));
         //email.sendKeys(data.get(0).get("Email"));
-        email.sendKeys("OfficeNT.3@gmail.com");
+        mail.sendKeys(email);
+        Thread.sleep(1000);
 
+        //WebElement emailConfirmation = driver.findElement(By.name("Nahomt50@mailnesia.com"));
         WebElement emailConfirmation = driver.findElement(By.name("ConfirmEmailAddress"));
         //emailConfirmation.sendKeys(data.get(0).get("ConfirmEmail"));
-        emailConfirmation.sendKeys("OfficeNT.3@gmail.com");
+        emailConfirmation.sendKeys(email);
+        Thread.sleep(1000);
 
-        WebElement password = driver.findElement(By.name("Password"));
+        //WebElement password = driver.findElement(By.name("1234.Tesfay"));
+        WebElement passwordElement = driver.findElement(By.name("Password"));
         //password.sendKeys(data.get(0).get("Password"));
-        password.sendKeys("2468.Tesfay");
+        passwordElement.sendKeys(password);
+        Thread.sleep(1000);
 
+        //WebElement passwordConfirmation = driver.findElement(By.name("1234.Tesfay"));
         WebElement passwordConfirmation = driver.findElement(By.name("ConfirmPassword"));
         //passwordConfirmation.sendKeys(data.get(0).get("ConfirmPassword"));
-        passwordConfirmation.sendKeys("2468.Tesfay");
-
+        passwordConfirmation.sendKeys(confirmpassword);
+        Thread.sleep(1000);
 
     }
 
@@ -75,29 +89,48 @@ public class MyStepdefs {
         //Check this out
         WebElement termsAndConditions = driver.findElement(By.cssSelector("label[for='sign_up_25']"));
         termsAndConditions.click();
+        Thread.sleep(500);
 
         WebElement ageConsent = driver.findElement(By.cssSelector("label[for='sign_up_26']"));
         ageConsent.click();
+        Thread.sleep(500);
 
         WebElement consent = driver.findElement(By.cssSelector("label[for='fanmembersignup_agreetocodeofethicsandconduct']"));
         consent.click();
+        Thread.sleep(500);
 
         WebElement join = driver.findElement(By.name("join"));
         join.click();
+        Thread.sleep(500);
     }
 
     @Then("I will get to the confirmation page")
     public void iWillGetToTheConfirmationPage() {
 
-        WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(10));
+         wait = new WebDriverWait(driver, Duration.ofSeconds(5));
 
-        wait.until(ExpectedConditions.urlContains("SignUpConfirmation"));
+       wait.until(ExpectedConditions.urlContains("SignUpConfirmation"));
 
         boolean urlContainsConfirmation = wait.until(ExpectedConditions.urlContains("SignUpConfirmation"));
         Assert.assertTrue("The URL does not contain 'SignUpConfirmation'", urlContainsConfirmation);
 
+    }
 
+    @Then("I will get error message")
+    public void iWillGetErrorMessage() {
 
+        wait = new WebDriverWait(driver, Duration.ofSeconds(5));
+
+        WebElement joinButton = driver.findElement(By.name("join"));
+
+        Assert.assertTrue("Error, the submitted information is wrong", joinButton.isEnabled());
+
+        List<WebElement> errorMessages = driver.findElements(By.cssSelector(".field-validation-error"));
+
+        boolean atLeastOneErrorVisible = errorMessages.stream().anyMatch(WebElement::isDisplayed);
+
+        Assert.assertTrue("Expected at least one error message to be visible, but none were.", atLeastOneErrorVisible);
+        
+        }
 
     }
-}
--- src/test/resources/features/account.feature
+++ src/test/resources/features/account.feature
@@ -1,22 +1,20 @@
 Feature: Create an account
 
-  Scenario:
+  Scenario: basic
     Given I am on the right page
-    When I put all the login info
+    When I put all the login info "14/04/2002", "Nahom", "Tesfay", "Nahomt59@mailnesia.com", "1234.Tesfay", "1234.Tesfay"
     And Click the right boxes
     Then I will get to the confirmation page
 
-  Scenario Outline:
+  Scenario Outline: dataDrivenFelscenario
     Given I am on the right page
-    When I put all the login info
-      | DateOfBirth | Forename    | Surname    | Email   |
-      | <dob>       | <firstName> | <LastName> | <Email> |
+    When I put all the login info "<DateOfBirth>", "<Forename>", "<Surname>", "<Email>", "<password>", "<confirmpassword>"
     And Click the right boxes
-    Then I will get to the confirmation page
+    Then I will get error message
 
 
     Examples:
-      | dob        | firstName | LastName | Email                |
-      | 14/13/2002 | Nahom     | Tesfay   | OfficeNT.3@gmail.com |
-      | 14/04/2002 | .         | Tesfay   | OfficeNT.3@gmail.com |
-      | 14/04/2002 | Nahom     | ?        | OfficeNT.3@gmail.com |
+      | DateOfBirth | Forename | Surname | Email                | password     | confirmpassword |
+      | 14/13/2002  | Nahom    | Tesfay  | OfficeNT.3@gmail.com | 12345.Tesfay | 1234.Tesfay     |
+      | 14/04/2002  |          | Tesfay  | OfficeNT.3@gmail.com | 1234.Tesfay  | 1234.Tesfay     |
+      | 14/04/2002  | Nahom    |         | OfficeNT.3@gmail.com | 1234.Tesfay  | 1234.Tesfay     |
